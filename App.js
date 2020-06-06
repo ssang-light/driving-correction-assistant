16fa4f77188c405d80afe41ed2e90933cace297f
younho9 <younho9.choo@gmail.com>
2020-06-06 21:53:03 +0900

    Implement Video & Location tracker

--- App.js
+++ App.js
@@ -1,106 +1,225 @@
-import React, { Component } from "react";
-import { Platform, Text, View, StyleSheet, Button } from "react-native";
-import Constants from "expo-constants";
-import * as Location from "expo-location";
-import * as Permissions from "expo-permissions";
+import React, { useState, useEffect, useRef } from 'react';
+import { Text, View, TouchableOpacity } from 'react-native';
+import { Camera } from 'expo-camera';
+import { Video } from 'expo-av';
+import * as FileSystem from 'expo-file-system';
+import * as MediaLibrary from 'expo-media-library';
+import * as Location from 'expo-location';
+import * as Permissions from 'expo-permissions';
+import { Ionicons } from '@expo/vector-icons';
 
 const GEOLOCATION_OPTIONS = {
   enableHighAccuracy: true,
   timeInterval: 10,
-  distanceInterval: 20
+  distanceInterval: 20,
 };
 
 let locationList = [];
 
-export default class App extends Component {
-  state = {
-    subscription: null,
-    currentLocation: null,
-    errorMessage: null,
-    isTracking: false
-  };
+export default function App() {
+  const [hasCameraPermission, setHasCameraPermission] = useState(null);
+  const [hasMediaLibraryPermission, setHasMediaLibraryPermission] = useState(
+    null,
+  );
+  const [hasLocationPermission, setHasLocationPermission] = useState(null);
+  const [cameraRef, setCameraRef] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+  const [recording, setRecording] = useState(false);
 
-  constructor(props) {
-    super(props);
-    if (Platform.OS === "android" && !Constants.isDevice) {
-      this.setState({
-        errorMessage:
-          "Oops, this will not work on Sketch in an Android emulator. Try it on your device!"
-      });
-    }
-  }
+  const [subscription, setSubscription] = useState(null);
+  const [currentLocation, setCurrentLocation] = useState(null);
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [isTracking, setIsTracking] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestPermissionsAsync();
+      setHasCameraPermission(status === 'granted');
+    })();
+  }, []);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await MediaLibrary.requestPermissionsAsync();
+      setHasMediaLibraryPermission(status === 'granted');
+    })();
+  }, []);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Location.requestPermissionsAsync();
+      setHasLocationPermission(status === 'granted');
+    })();
+  }, []);
 
   _stopTracking = async () => {
-    this.state.subscription.remove();
-    this.setState({
-      currentLocation: null,
-      subscription: null,
-      isTracking: false
-    });
+    setSubscription(null);
+    setCurrentLocation(null);
+    setSubscription(null);
+    setIsTracking(false);
     console.log(locationList);
   };
 
   _watchLocationAsync = async () => {
-    let { status } = await Permissions.askAsync(Permissions.LOCATION);
-    if (status !== "granted") {
-      this.setState({
-        errorMessage: "Permission to access location was denied"
-      });
-    }
-    this.setState({
-      isTracking: true
-    });
+    setIsTracking(true);
+
     let subscription = await Location.watchPositionAsync(
       GEOLOCATION_OPTIONS,
-      location => {
-        this.setState({
-          currentLocation: location
-        });
+      (location) => {
+        setCurrentLocation(location);
         locationList.push(location);
-      }
+      },
     );
-    this.setState({ subscription });
+    setSubscription(subscription);
   };
 
-  render() {
-    let text = "Waiting..";
-    if (this.state.errorMessage) {
-      text = this.state.errorMessage;
-    } else if (this.state.currentLocation) {
-      text = JSON.stringify(this.state.currentLocation);
-    }
-
-    return (
-      <View style={styles.container}>
-        <Text style={styles.paragraph}>{text}</Text>
-        <Button
-          style={styles.button}
-          title={this.state.isTracking ? "Stop tracking" : "Start tracking"}
-          onPress={
-            this.state.isTracking
-              ? () => this._stopTracking()
-              : () => this._watchLocationAsync()
-          }
-        />
-      </View>
-    );
+  if (hasCameraPermission === null) {
+    return <View />;
   }
+
+  if (hasCameraPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
+  if (hasMediaLibraryPermission === null) {
+    return <View />;
+  }
+
+  if (hasMediaLibraryPermission === false) {
+    return <Text>No access to MediaLibrary</Text>;
+  }
+
+  if (hasLocationPermission === null) {
+    return <View />;
+  }
+
+  if (hasLocationPermission === false) {
+    return <Text>No access to Location</Text>;
+  }
+
+  return (
+    <View style={{ flex: 1 }}>
+      <Camera
+        style={{ flex: 1 }}
+        type={type}
+        ref={(ref) => {
+          setCameraRef(ref);
+        }}
+      >
+        <View
+          style={{
+            flex: 1,
+            backgroundColor: 'transparent',
+            justifyContent: 'flex-end',
+          }}
+        >
+          <View
+            style={{
+              display: 'flex',
+              flexDirection: 'row',
+              justifyContent: 'space-evenly',
+            }}
+          >
+            <TouchableOpacity
+              style={{
+                flex: 0.1,
+                alignSelf: 'flex-end',
+              }}
+              onPress={() => {
+                setType(
+                  type === Camera.Constants.Type.back
+                    ? Camera.Constants.Type.front
+                    : Camera.Constants.Type.back,
+                );
+              }}
+            >
+              <Ionicons
+                name={
+                  Platform.OS === 'ios'
+                    ? 'ios-reverse-camera'
+                    : 'md-reverse-camera'
+                }
+                size={40}
+                color="white"
+              />
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={{ alignSelf: 'center' }}
+              onPress={async () => {
+                if (cameraRef) {
+                  let photo = await cameraRef.takePictureAsync();
+                  console.log('photo', photo);
+                  let asset = await MediaLibrary.createAssetAsync(photo.uri);
+                }
+              }}
+            >
+              <View
+                style={{
+                  borderWidth: 2,
+                  borderRadius: '50%',
+                  borderColor: 'white',
+                  height: 50,
+                  width: 50,
+                  display: 'flex',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                }}
+              >
+                <View
+                  style={{
+                    borderWidth: 2,
+                    borderRadius: '50%',
+                    borderColor: 'white',
+                    height: 40,
+                    width: 40,
+                    backgroundColor: 'white',
+                  }}
+                ></View>
+              </View>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={{ alignSelf: 'center' }}
+              onPress={async () => {
+                if (!recording) {
+                  setRecording(true);
+                  let video = await cameraRef.recordAsync();
+                  console.log('video', video);
+                  this._watchLocationAsync();
+                  let asset = await MediaLibrary.createAssetAsync(video.uri);
+                } else {
+                  setRecording(false);
+                  cameraRef.stopRecording();
+                  this._stopTracking();
+                }
+              }}
+            >
+              <View
+                style={{
+                  borderWidth: 2,
+                  borderRadius: '50%',
+                  borderColor: 'red',
+                  height: 50,
+                  width: 50,
+                  display: 'flex',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                }}
+              >
+                <View
+                  style={{
+                    borderWidth: 2,
+                    borderRadius: '50%',
+                    borderColor: recording ? 'blue' : 'red',
+                    height: 40,
+                    width: 40,
+                    backgroundColor: recording ? 'blue' : 'red',
+                  }}
+                ></View>
+              </View>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Camera>
+    </View>
+  );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    paddingTop: Constants.statusBarHeight,
-    backgroundColor: "#ecf0f1"
-  },
-  paragraph: {
-    margin: 24,
-    fontSize: 18,
-    textAlign: "center"
-  },
-  button: {
-    backgroundColor: "blue"
-  }
-});
